9cf749838e0f5e45f9c4734fb8589e9d60241222
loverboy <loverboykosu@gmail.com>
2025-06-30 21:57:13 +0900

    feat: #40 lecture add framework

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <>
+      <div className="topbarContaier">
+        <div className="topbarLeft">
+          <span className="logo">Real SNS</span>
+        </div>
+        <div className="topbarCenter">
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="探し物はなんですか？"
+          />
+        </div>
+        <div className="topbarRight">
+          <div className="topbarIconItem">1</div>
+          <div className="topbarIconItem">2</div>
+        </div>
+        <img src="/assets/persion/1.jpg" alt="" className="topbarImg" />
+      </div>
+    </>
+  );
 }
