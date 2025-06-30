3fb68ed48c30994ec7911a3298776a5c1f64cf5a
loverboy <loverboykosu@gmail.com>
2025-06-30 23:43:28 +0900

    feat: #42 lecture

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import SearchIcon from "@mui/icons-material/Search";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 export default function Topbar() {
   return (
     <>
@@ -8,6 +10,7 @@ export default function Topbar() {
           <span className="logo">Real SNS</span>
         </div>
         <div className="topbarCenter">
+          <SearchIcon className="searchIcon" />
           <input
             type="text"
             className="searchInput"
@@ -15,8 +18,14 @@ export default function Topbar() {
           />
         </div>
         <div className="topbarRight">
-          <div className="topbarIconItem">1</div>
-          <div className="topbarIconItem">2</div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBa">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBa">2</span>
+          </div>
         </div>
         <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
       </div>
